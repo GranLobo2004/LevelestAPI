07a9b2c3723581830c541e7dbd3ef4ac0b4be8a2
GranLobo2004 <146849588+GranLobo2004@users.noreply.github.com>
2025-11-03 13:23:56 +0100

    Main Application

---
+++ src/main/java/es/zenith/levelestapi/LevelestApiApplication.java
@@ -0,0 +1,13 @@
+package com.Zenith.levelestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevelestApiApplication.class, args);
+    }
+
+}
